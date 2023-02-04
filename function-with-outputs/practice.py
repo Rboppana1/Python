53f9b19ab159787ae61cdb2e7f9fab32f01e65fd
Rboppana1 <boppana.rl@gmail.com>
2023-02-04 13:04:12 +0530

    Python

--- function-with-outputs/practice.py
+++ function-with-outputs/practice.py
@@ -1,13 +1,25 @@
+"""
 def format_name(first_name, last_name):
     f_name = first_name.capitalize()
     l_name = last_name.capitalize()
     full_name = f_name + " " + l_name
     print(full_name)
 
-
 format_name("peter", "zang")
+"""
+
+def format_name(first_name, last_name):
+    formated_f_name = first_name.title()
+    formated_l_name = last_name.title()
+    #full_name = formated_f_name + " " + formated_l_name
+    return f"({formated_f_name} {formated_l_name})"
+    #print(full_name)
+
+formatted_name = format_name("sample", "simple")
+print(formatted_name)
 
 
+"""
 def format_name(first_name, last_name):
     f_name = first_name.capitalize()
     l_name = last_name.capitalize()
@@ -18,3 +30,32 @@ def format_name(first_name, last_name):
 print(format_name("rayi", "error"))
 
 # format_name("peter","zang")
+
+"""
+
+length = len(formatted_name)
+print(length)
+
+def format_name(f_name, l_name):
+    if f_name == "test" or l_name == "":
+        return f_name
+format_name("test", "test")
+print("First name is {}"(format_name))
+"""
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    #return f"Result: {formated_f_name} {formated_l_name}"
+    return f"({formated_f_name} {formated_l_name})"
+"""
+
+def add(a, b):
+    # returning sum of a and b
+    return a + b
+def is_true(a):
+    # returning boolean of a
+    return bool(a)
+# calling function
+res = add(2, 3)
+print("Result of add function is {}".format(res))
+res = is_true(2 < 5)
+print("\nResult of is_true function is {}".format(res))
--- practice-projects/auction-program.py
+++ practice-projects/auction-program.py
@@ -1,1 +1,18 @@
-#In progress
+logo = '''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+'''
+
+#HINT: You can call clear() to clear the output in the console.
+print (logo)
+Name = input("Enter your name:")
+BidPrice = input("Enter bid price:")
