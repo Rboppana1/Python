87d24e79381704bcf4a8562e0b57ea1afb4e0591
Rboppana1 <boppana.rl@gmail.com>
2023-03-06 21:25:38 +0530

    Day11

--- practice-projects/blackjack/blackjack-one.py
+++ practice-projects/blackjack/blackjack-one.py
@@ -25,6 +25,7 @@
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
 #cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
 #Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
 #user_cards = []
 #computer_cards = []
@@ -53,3 +54,46 @@ logo = """
       |  \/ K|                            _/ |                
       `------'                           |__/           
 """
+import random
+
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+   # if 11 in cards and 10 in cards and len(cards) == 2:
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)
+
+user_cards = []
+computer_cards = []
+is_game_over = False
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+  # new_card = deal_card()
+   #user_cards.append(new_card)
+
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+print(f" Your cards: {user_cards}, current score: {user_score}")
+print(f" Computer's first card: {computer_cards[0]}")
+
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
+else:
+    user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+    if user_should_deal == "y":
+        user_cards.append(deal_card())
+    else:
+        is_game_over = True
+
+
